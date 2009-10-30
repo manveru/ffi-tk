c23107166bb3ceb23a473844bee4e026020ccc75
Michael Fellinger <m.fellinger@gmail.com>
2009-10-31 00:59:13 +0900

    Add methods for Pack

--- lib/ffi-tk/command/pack.rb
+++ lib/ffi-tk/command/pack.rb
@@ -1,4 +1,9 @@
 module Tk
+  # Geometry manager that packs around edges of cavity
+  #
+  # The pack command is used to communicate with the packer, a geometry manager
+  # that arranges the children of a parent by packing them in order around the
+  # edges of the parent.
   module Pack
     # If the first argument to pack is a window name (any value starting with
     # "."), then the command is processed in the same way as [configure].
@@ -6,9 +11,73 @@ module Tk
       Tk.execute('pack', *args)
     end
 
+    # The arguments consist of the names of one or more slave windows followed
+    # by a hash of arguments that specify how to manage the slaves.
+    # See THE PACKER ALGORITHM for details on how the options are used by the
+    # packer.
+    def self.configure(*arguments)
+      Tk.execute('pack', 'configure', *arguments)
+    end
+
+    # Removes each of the +slaves+ from the packing order for its master and
+    # unmaps their windows.
+    # The +slaves+ will no longer be managed by the packer.
+    def self.forget(*slaves)
+      Tk.execute_only('pack', 'forget', *slaves)
+    end
+
+    # Returns a list whose elements are the current configuration state of the
+    # +slave+ given by in the same option-value form that might be specified
+    # to pack configure.
+    # The first two elements of the list are `in: master` where master is the
+    # +slave+'s master.
+    def self.info(slave)
+      Tk.execute('pack', 'info', slave)
+    end
+
+    # If +boolean+ is true then propagation is enabled for +master+, which must
+    # be a window (see GEOMETRY PROPAGATION below).
+    # If +boolean+ is false, then propagation is disabled for +master+.
+    # In either of these cases nil is returned.
+    # If boolean is omitted then the command returns 0 or 1 to indicate whether
+    # propagation is currently enabled for master.
+    # Propagation is enabled by default.
+    def self.propagate(master, boolean = true)
+      Tk.execute_only('pack', 'propagate', master, boolean)
+    end
+
+    # Returns a list of all of the slaves in the packing order for master. The
+    # order of the slaves in the list is the same as their order in the packing
+    # order.
+    def self.slaves(master)
+      Tk.execute('pack', 'slaves', master)
+    end
+
     def pack(options = {})
-      Tk.execute('pack', self, options)
+      Pack.pack(self, options)
       self
+    end
+
+    def pack_configure(options = {})
+      Tk.execute_only('pack', 'configure', self, options)
+      self
+    end
+
+    def pack_forget
+      Pack.forget(self)
+      self
+    end
+
+    def pack_info
+      Pack.info(self)
+    end
+
+    def pack_propagate(boolean = true)
+      Pack.propagate(self, boolean)
+    end
+
+    def pack_slaves
+      Pack.slaves(self)
     end
   end
 end
