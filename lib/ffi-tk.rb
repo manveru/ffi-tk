fa36ee14dd6c3ddc84da9cb5f5e59446b56cb906
Michael Fellinger <m.fellinger@gmail.com>
2009-10-30 23:44:35 +0900

    Put command requires into a single place

--- lib/ffi-tk.rb
+++ lib/ffi-tk.rb
@@ -11,21 +11,15 @@ module Tk
   OK = 0
   ERROR = 1
 
-  autoload :Bind,       'ffi-tk/command/bind'
   autoload :Button,     'ffi-tk/widget/button'
-  autoload :Destroy,    'ffi-tk/command/destroy'
   autoload :Entry,      'ffi-tk/widget/entry'
-  autoload :EvalResult, 'ffi-tk/eval_result'
   autoload :Event,      'ffi-tk/event'
-  autoload :Pack,       'ffi-tk/command/pack'
   autoload :Root,       'ffi-tk/widget/root'
   autoload :Text,       'ffi-tk/widget/text'
   autoload :Widget,     'ffi-tk/widget'
 
   # Don't autoload this, or find out why it segfaults
-  require 'ffi-tk/command/cget'
-  require 'ffi-tk/command/focus'
-  require 'ffi-tk/command/configure'
+  require 'ffi-tk/command'
 
   # require the real thing
   require 'ffi-tk/tk'
---
+++ lib/ffi-tk/command.rb
@@ -0,0 +1,12 @@
+module Tk
+  require 'ffi-tk/command/bindtags'
+  require 'ffi-tk/command/bell'
+  require 'ffi-tk/command/bind'
+  require 'ffi-tk/command/bindtags'
+  require 'ffi-tk/command/cget'
+  require 'ffi-tk/command/configure'
+  require 'ffi-tk/command/destroy'
+  require 'ffi-tk/command/focus'
+  require 'ffi-tk/command/image'
+  require 'ffi-tk/command/pack'
+end
