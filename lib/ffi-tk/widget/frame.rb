1f26b50a78b38fefe9928f2d4ca7839fd15b7cd5
Michael Fellinger <m.fellinger@gmail.com>
2009-11-03 00:20:09 +0900

    Add frame widget

--- lib/ffi-tk/widget/frame.rb
+++ lib/ffi-tk/widget/frame.rb
@@ -1,4 +1,9 @@
 module Tk
+  # Create and manipulate frame widgets
+  # A frame is a simple widget.
+  # Its primary purpose is to act as a spacer or container for complex window
+  # layouts. The only features of a frame are its background color and an
+  # optional 3-D border to make the frame appear raised or sunken.
   class Frame < Widget
     include Cget, Configure
 
