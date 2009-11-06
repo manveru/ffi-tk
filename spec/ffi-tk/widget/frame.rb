70b07ccf49293ba94d2a8dd1fed56567f7570b55
Michael Fellinger <m.fellinger@gmail.com>
2009-11-07 03:01:31 +0900

    Move specs for widgets into own directory

--- spec/ffi-tk/widget/frame.rb
+++ spec/ffi-tk/widget/frame.rb
@@ -1,10 +1,8 @@
 require_relative '../helper'
 
-Tk.init
-
 describe Tk::Frame do
   it 'creates a new frame' do
-    @frame = Tk::Frame.new(Tk.root)
+    @frame = Tk::Frame.new
     @frame.winfo_exists.should == true
   end
 end
