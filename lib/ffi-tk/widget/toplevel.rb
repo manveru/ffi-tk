bb0a887d7ad2f128b6efb3aa1417472b0d1d874b
Michael Fellinger <m.fellinger@gmail.com>
2009-11-07 03:00:18 +0900

    Add last missing widget: Toplevel

---
+++ lib/ffi-tk/widget/toplevel.rb
@@ -0,0 +1,12 @@
+module Tk
+  # A toplevel is similar to a frame except that it is created as a top-level
+  # window: its X parent is the root window of a screen rather than the logical
+  # parent from its path name.
+  # The primary purpose of a toplevel is to serve as a container for dialog
+  # boxes and other collections of widgets.
+  # The only visible features of a toplevel are its background color and an
+  # optional 3-D border to make the toplevel appear raised or sunken.
+  class Toplevel < Widget
+    include Cget, Configure
+  end
+end
