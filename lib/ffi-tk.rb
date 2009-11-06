4e6549388dc7f21f7d21de31800963da8f9cb388
Michael Fellinger <m.fellinger@gmail.com>
2009-11-07 03:25:04 +0900

    Add autoloads for all widgets

--- lib/ffi-tk.rb
+++ lib/ffi-tk.rb
@@ -10,6 +10,7 @@ module Tk
   OK = 0
   ERROR = 1
   None = Object.new
+
   class << None
     def to_tcl; nil; end
     def to_tcl_options?; self; end
@@ -17,23 +18,33 @@ module Tk
     def inspect; '#<None>'; end
   end
 
-  autoload :Button,       'ffi-tk/widget/button'
-  autoload :Canvas,       'ffi-tk/widget/canvas'
-  autoload :Entry,        'ffi-tk/widget/entry'
-  autoload :Frame,        'ffi-tk/widget/frame'
-  autoload :Label,        'ffi-tk/widget/label'
-  autoload :Menu,         'ffi-tk/widget/menu'
-  autoload :Root,         'ffi-tk/widget/root'
-  autoload :Text,         'ffi-tk/widget/text'
-  autoload :Widget,       'ffi-tk/widget'
+  autoload :Button,      'ffi-tk/widget/button'
+  autoload :Canvas,      'ffi-tk/widget/canvas'
+  autoload :CheckButton, 'ffi-tk/widget/checkbutton'
+  autoload :Entry,       'ffi-tk/widget/entry'
+  autoload :Frame,       'ffi-tk/widget/frame'
+  autoload :Label,       'ffi-tk/widget/label'
+  autoload :LabelFrame,  'ffi-tk/widget/labelframe'
+  autoload :Listbox,     'ffi-tk/widget/listbox'
+  autoload :Menu,        'ffi-tk/widget/menu'
+  autoload :MenuButton,  'ffi-tk/widget/menubutton'
+  autoload :Message,     'ffi-tk/widget/message'
+  autoload :PanedWindow, 'ffi-tk/widget/panedwindow'
+  autoload :RadioButton, 'ffi-tk/widget/radiobutton'
+  autoload :Root,        'ffi-tk/widget/root'
+  autoload :Scale,       'ffi-tk/widget/scale'
+  autoload :Scrollbar,   'ffi-tk/widget/scrollbar'
+  autoload :Spinbox,     'ffi-tk/widget/spinbox'
+  autoload :Text,        'ffi-tk/widget/text'
+  autoload :Toplevel,    'ffi-tk/widget/toplevel'
 
-  # Don't autoload this, or find out why it segfaults
   require 'ffi-tk/core_extensions'
   require 'ffi-tk/geometry'
   require 'ffi-tk/event/data'
   require 'ffi-tk/event/handler'
   require 'ffi-tk/variable'
   require 'ffi-tk/command'
+  require 'ffi-tk/widget'
 
   # require the real thing
   require 'ffi-tk/tk'
