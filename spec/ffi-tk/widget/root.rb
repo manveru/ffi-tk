394ce7ef0a6c610cc8568c0b3870d4cdda7d2e58
Michael Fellinger <m.fellinger@gmail.com>
2009-11-07 03:36:16 +0900

    Fix spec of Tk::Root

--- spec/ffi-tk/widget/root.rb
+++ spec/ffi-tk/widget/root.rb
@@ -4,6 +4,6 @@ describe Tk::Root do
   it 'initializes' do
     instance = Tk::Root.new
     instance.class.should == Tk::Root
-    instance.parent.should == Tk.root
+    instance.parent.should == nil
   end
 end
